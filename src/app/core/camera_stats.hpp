605855228c4f55431b647553e86c1fe60e1b0e3d
Fabien Bessy <fabien.bessy@gmail.com>
2026-04-23 21:13:39 +0200

    Modern initialization.

--- src/app/core/camera_stats.hpp
+++ src/app/core/camera_stats.hpp
@@ -8,14 +8,14 @@ namespace fluvel_app
 
 struct CameraStats
 {
-    double capturedFps = 0.0;
-    double processedFps = 0.0;
-    double displayedFps = 0.0;
-    double dropRate = 0.0;
-    double avgLatencyDisplayMs = 0.0;
-    double maxLatencyDisplayMs = 0.0;
-    double avgLatencyProcMs = 0.0;
-    double avgContourSize = 0.0;
+    double capturedFps{0.0};
+    double processedFps{0.0};
+    double displayedFps{0.0};
+    double dropRate{0.0};
+    double avgLatencyDisplayMs{0.0};
+    double maxLatencyDisplayMs{0.0};
+    double avgLatencyProcMs{0.0};
+    double avgContourSize{0.0};
 };
 
 } // namespace fluvel_app
